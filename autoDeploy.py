4205dfb2b3efec6b88f94ce0fbb0ed3cf566a466
Iván Pérez <91698324+huzmgod@users.noreply.github.com>
2023-03-09 12:30:14 +0100

    Update autoDeploy.py

--- autoDeploy.py
+++ autoDeploy.py
@@ -6,9 +6,13 @@ clusterName= input("Introduce your cluster name (default 'trainingPath'): ") or 
 # Define the commands to be executed
 commands = {
     "Part 0: Download the materials": [
+        f"mkdir {clusterName}",
+        f"cd {clusterName}",
         "git clone https://github.com/huzmgod/k8sTraining.git",
-        "cd k8sTraining",
+        "mv k8sTraining/* .",
+        "rm -rf k8sTraining"
     ],
+    
     "Part 1: Apache and nginx": [
         f"kind create cluster --name={clusterName} --config=cluster-config.yaml",
         "kubectl apply -k .",
@@ -16,7 +20,7 @@ commands = {
     "Part 2: Deploying the dashboard": [
         "kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.7.0/aio/deploy/recommended.yaml --validate=false",
         "kubectl apply -f jenkins.yaml",
-        "kubectl get pod -n kubernetes-dashboard",
+        # "kubectl get pods -n kubernetes-dashboard",
         "kubectl create serviceaccount -n kubernetes-dashboard admin-user",
         "kubectl create clusterrolebinding -n kubernetes-dashboard admin-user --clusterrole cluster-admin --serviceaccount=kubernetes-dashboard:admin-user",
         # Get the token manually
@@ -28,8 +32,10 @@ commands = {
         # "firefox http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/#/login",
     ],
     "Part 3: Deploying Istio": [
+        
         "curl -L https://istio.io/downloadIstio | sh -",
-        "cd istio-*",
+        #TODO: Check if the version is the same. If not, change the following line accordingly
+        "cd istio-1.17.1",
         "export PATH=$PWD/bin:$PATH",
         # Execute manually
         # "kubectl get crds | grep 'istio.io\|certmanager.k8s.io' | wc -l",
@@ -42,8 +48,8 @@ commands = {
         # check manually
         # kubectl get svc -n istio-system
         # echo "INGRESS_HOST=$INGRESS_HOST, INGRESS_PORT=$INGRESS_PORT
-        "export GATEWAY_URL=$INGRESS_HOST:$INGRESS_PORT",
-        "cd ..",
+        "export GATEWAY_URL=$INGRESS_HOST:$INGRESS_PORT", 
+       
     ],
     "Part 4: Installing Kiali": [
         "kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.17/samples/addons/kiali.yaml --validate=false",
@@ -57,4 +63,11 @@ for title, command_list in commands.items():
     print(f"Executing {title}")
     for command in command_list:
         print(f"Executing: {command}")
-        os.system(command)
+        if (command.startswith("cd")):
+            os.chdir(command.split(" ")[1])
+            print(f"Current directory: {os.getcwd()}")
+        elif (command.startswith("export")):
+            os.environ[command.split("=")[0].split(" ")[1]] = command.split("=")[1]
+            # print(f"Current environment: {os.environ}")
+        else:
+            os.system(command)
